b3fed4efec8b24a90b4ddbfef218fb835cc687a8
smiroczn <smiroczn@umd.edu>
2026-06-22 11:18:38 -0400

    Update SLMButton.m
    modified

--- tools/real_time_analysis/SLMButton.m
+++ tools/real_time_analysis/SLMButton.m
@@ -1,80 +1,83 @@
 assignin("base","target_cells",target_cells);
-            writematrix(target_cells,'stimulationTargets.csv');
+writematrix(target_cells,'stimulationTargets.csv');
 
-            if app.SLM_ON && app.SLM == "BNS" % Boulder Nonlinear Systems
-                if stimParams.seqMode
-%                     phaseMasks = cell(1,size(target_cells,1));
-%                     for i = 1:size(target_cells,1)
-%                         [phaseMasks{i},~,~] = stimulateBNS_SLM(target_cells(i,:), stimParams); % send commands to BNS SLM
-%                     end
-                    if size(target_cells_group2,1) > 0 % if there are two groups for stimulation
+% NEW: Extract eroded masks for stimulation (add at the beginning)
+erosionPixels = 2;  % Adjust as needed (1-2 pixels recommended)
+targetCellMasks = extractErodedMasks(app.cell_masks, cellIDs, erosionPixels);
+targetCellMasks_group2 = extractErodedMasks(app.cell_masks, cellIDs_group2, erosionPixels);
 
-                        nTargets = 2; % hard coded for now, assuming there are only two groups
+if app.SLM_ON && app.SLM == "BNS" % Boulder Nonlinear Systems
+    if stimParams.seqMode
+        if size(target_cells_group2,1) > 0 % if there are two groups for stimulation
 
-                        [phaseMask1,msk1,I1] = stimulateBNS_SLM(target_cells, stimParams); % send commands to BNS SLM
-                        pause(1)
-                        [phaseMask2,~,~] = stimulateBNS_SLM(target_cells_group2, stimParams); % send commands to BNS SLM
-                        pause(1)
+            nTargets = 2; % hard coded for now, assuming there are only two groups
 
-                        image_size = size(phaseMask1);
-    
-                        images_flat(:,1) = reshape(phaseMask1,image_size(1)*image_size(2),1);
-                        images_flat(:,2) = reshape(phaseMask2,image_size(1)*image_size(2),1);
-                        
-                        image_sequence = reshape(images_flat,image_size(1)*image_size(2)*2,1); % stimulate 2 groups of cells in sequence
-    
-                        disp('Downloading images... please wait.');
-                        max_OD_frames = 20;
-                        download_error = calllib(app.dll_name, 'Download_image_sequence', app.board_number, nTargets, image_sequence, max_OD_frames);
-                        if download_error
-                            disp('Failed to download images.');
-                        else
-                            disp('Done downloading images.');
-                            app.StimulateButton.Enable = 1;
-                            imshow(msk1,'Parent', app.UIAxes8); % for now only display the phase mask corresponding to the first vortex
-                            imshow(I1,'Parent', app.UIAxes8_2);
-                        end
+            % MODIFIED: Pass masks and exptVars
+            [phaseMask1,msk1,I1] = stimulateBNS_SLM(target_cells, stimParams, targetCellMasks, app.exptVars);
+            pause(1)
+            [phaseMask2,~,~] = stimulateBNS_SLM(target_cells_group2, stimParams, targetCellMasks_group2, app.exptVars);
+            pause(1)
 
-                    else
-                    
-                        [phaseMask1,msk1,I1] = stimulateBNS_SLM(target_cells(1,:), stimParams); % send commands to BNS SLM
-                        pause(1)
-                        [phaseMask2,~,~] = stimulateBNS_SLM(target_cells(2,:), stimParams); % send commands to BNS SLM
-                        pause(1)
-                        if nTargets > 2
-                            [phaseMask3,~,~] = stimulateBNS_SLM(target_cells(3,:), stimParams); % send commands to BNS SLM
-                            if nTargets == 4
-                                [phaseMask4,~,~] = stimulateBNS_SLM(target_cells(4,:), stimParams); % send commands to BNS SLM
-                            end
-                        end
-    
-                        image_size = size(phaseMask1);
-    
-                        images_flat(:,1) = reshape(phaseMask1,image_size(1)*image_size(2),1);
-                        images_flat(:,2) = reshape(phaseMask2,image_size(1)*image_size(2),1);
-    
-                        if nTargets == 4
-                            images_flat(:,3) = reshape(phaseMask3,image_size(1)*image_size(2),1);
-                            images_flat(:,4) = reshape(phaseMask4,image_size(1)*image_size(2),1);
-                            image_sequence = reshape(images_flat,image_size(1)*image_size(2)*4,1); % stimulate 4 cells in sequence
-                        elseif nTargets == 3
-                            images_flat(:,3) = reshape(phaseMask3,image_size(1)*image_size(2),1);
-                            image_sequence = reshape(images_flat,image_size(1)*image_size(2)*3,1); % stimulate 3 cells in sequence
-                        else
-                            image_sequence = reshape(images_flat,image_size(1)*image_size(2)*2,1); % stimulate 2 cells in sequence
-                        end
-    
-                        disp('Downloading images... please wait.');
-                        max_OD_frames = 20;
-                        download_error = calllib(app.dll_name, 'Download_image_sequence', app.board_number, nTargets, image_sequence, max_OD_frames);
-                        if download_error
-                            disp('Failed to download images.');
-                        else
-                            disp('Done downloading images.');
-                            app.StimulateButton.Enable = 1;
-                            imshow(msk1,'Parent', app.UIAxes8); % for now only display the phase mask corresponding to the first vortex
-                            imshow(I1,'Parent', app.UIAxes8_2);
-                        end
-                    end
+            image_size = size(phaseMask1);
+
+            images_flat(:,1) = reshape(phaseMask1,image_size(1)*image_size(2),1);
+            images_flat(:,2) = reshape(phaseMask2,image_size(1)*image_size(2),1);
+            
+            image_sequence = reshape(images_flat,image_size(1)*image_size(2)*2,1);
+
+            disp('Downloading images... please wait.');
+            max_OD_frames = 20;
+            download_error = calllib(app.dll_name, 'Download_image_sequence', app.board_number, nTargets, image_sequence, max_OD_frames);
+            if download_error
+                disp('Failed to download images.');
+            else
+                disp('Done downloading images.');
+                app.StimulateButton.Enable = 1;
+                imshow(msk1,'Parent', app.UIAxes8);
+                imshow(I1,'Parent', app.UIAxes8_2);
+            end
+
+        else
+        
+            % MODIFIED: Pass individual cell masks
+            [phaseMask1,msk1,I1] = stimulateBNS_SLM(target_cells(1,:), stimParams, {targetCellMasks{1}}, app.exptVars);
+            pause(1)
+            [phaseMask2,~,~] = stimulateBNS_SLM(target_cells(2,:), stimParams, {targetCellMasks{2}}, app.exptVars);
+            pause(1)
+            if nTargets > 2
+                [phaseMask3,~,~] = stimulateBNS_SLM(target_cells(3,:), stimParams, {targetCellMasks{3}}, app.exptVars);
+                if nTargets == 4
+                    [phaseMask4,~,~] = stimulateBNS_SLM(target_cells(4,:), stimParams, {targetCellMasks{4}}, app.exptVars);
                 end
             end
+
+            image_size = size(phaseMask1);
+
+            images_flat(:,1) = reshape(phaseMask1,image_size(1)*image_size(2),1);
+            images_flat(:,2) = reshape(phaseMask2,image_size(1)*image_size(2),1);
+
+            if nTargets == 4
+                images_flat(:,3) = reshape(phaseMask3,image_size(1)*image_size(2),1);
+                images_flat(:,4) = reshape(phaseMask4,image_size(1)*image_size(2),1);
+                image_sequence = reshape(images_flat,image_size(1)*image_size(2)*4,1);
+            elseif nTargets == 3
+                images_flat(:,3) = reshape(phaseMask3,image_size(1)*image_size(2),1);
+                image_sequence = reshape(images_flat,image_size(1)*image_size(2)*3,1);
+            else
+                image_sequence = reshape(images_flat,image_size(1)*image_size(2)*2,1);
+            end
+
+            disp('Downloading images... please wait.');
+            max_OD_frames = 20;
+            download_error = calllib(app.dll_name, 'Download_image_sequence', app.board_number, nTargets, image_sequence, max_OD_frames);
+            if download_error
+                disp('Failed to download images.');
+            else
+                disp('Done downloading images.');
+                app.StimulateButton.Enable = 1;
+                imshow(msk1,'Parent', app.UIAxes8);
+                imshow(I1,'Parent', app.UIAxes8_2);
+            end
+        end
+    end
+end
